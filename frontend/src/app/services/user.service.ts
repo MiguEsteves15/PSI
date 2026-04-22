eb7b5d54f672d2fac2d6044ace3caea32547519e
João Oliveira <fc61873@alunos.fc.ul.pt>
2026-04-22 16:51:23 +0100

    Limpeza e inicialização do frontend

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    private apiUrl = 'http://localhost:3000/api/users/me';
+
+    constructor(private http: HttpClient) { }
+
+    // US3
+    getProfile(): Observable<any> {
+        return this.http.get(this.apiUrl);
+    }
+
+    // US4
+    updateUsername(newUsername: string): Observable<any> {
+        return this.http.put(`${this.apiUrl}/username`, { username: newUsername });
+    }
+
+    // US4
+    updatePassword(currentPassword: string, newPassword: string): Observable<any> {
+        return this.http.put(`${this.apiUrl}/password`, { currentPassword, newPassword });
+    }
+}
